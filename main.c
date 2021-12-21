f60e17a53d0d918245cb86044ffdc1a2dd6b2a67
ahmed-ayman-elmelgy <ahmed.ayman.abdellatif@gmail.com>
2021-12-21 14:58:12 +0200

    fixing bugs at leatest version

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ int32_t Fun_Arithmetic( int32_t First_Num , int32_t Second_Num , int8_t operatio
 			break ; 	
 		default : 
 				printf("wrong operation /n") ;
+				return 0 ;
 			break ;			
 	}
 	
